e8bba5ad92f2e000efc0e74c2070fdf3390f7ec6
jiajing-2000 <candymagic2000@qq.com>
2023-03-17 20:25:38 -0400

    init

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,61 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  arr.sort!
+  arr[-1] + arr[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? || arr.length == 1
+  arr.each_with_index do |num, index|
+    arr.each_with_index do |num2, index2|
+      if index != index2 && num + num2 == n
+        return true
+      end
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /\A[01]+\z/
+    s.to_i(2) % 4 == 0
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
